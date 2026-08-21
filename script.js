ce6993e34d7e2b6fbcd4ba2b9dcad0c3533e3542
geotamayo2900 <geovanniencina@gmail.com>
2026-08-21 10:35:38 -0400

    Enhance button interactions with emojis
    Updated button click responses and added emojis.

--- script.js
+++ script.js
@@ -24,15 +24,15 @@ botonNo.addEventListener("mouseover", function(){
 botonNo.addEventListener("click", function(){
     intentos++;
     if (intentos === 1) {
-        titulo.textContent = "en serio no quieres?";
+        titulo.textContent = "en serio no quieres? 🥺😭";
     }
 
     if (intentos === 2) {
-        titulo.textContent = "Porfavor porfavor acepta";
+        titulo.textContent = "Porfavor porfavor acepta 🥺";
     }
 
     if (intentos === 3){
-        titulo.textContent = "pues ahora me aceptas";
+        titulo.textContent = "pues ahora me aceptas 😣";
     }
 });
 
@@ -55,12 +55,12 @@ botonNo.addEventListener("touchstart", function(event){
 });
 
 botonSi.addEventListener("click", function(){
-    titulo.textContent = "sabia que aceptarias";
+    titulo.textContent = "sabia que aceptarias jeje 😏";
 
     botonSi.style.display = "none";
     botonNo.style.display = "none";
 
-    mensajeFinal.textContent = "Entonces tenemos una cita pendiente";
+    mensajeFinal.textContent = "Entonces tenemos una cita pendiente 😘";
 
     for (let i = 0; i < 20; i++) {
         crearCorazon();
